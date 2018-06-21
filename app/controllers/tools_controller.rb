9dccdb88eb8a224fcdb717d21e93e3fd2ff79da1
Victor Lacombe <lacombe.victor@hotmail.fr>
2018-06-21 10:56:57 +0200

    setting the models, controllers and seeding the db

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,6 @@
+class ToolsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+  def index
+    @tools = Tool.all
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seed Starts"
+
+Tool.create!(name: "Zapier", description: "The easiest way to automate your work", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU2B0bAgzdduS4ow2H_Mm482ViCEnrPMUAc3GfvxzakGAS0Cil",
+              website_url: "https://zapier.com/", chrome_extension_url: "https://chrome.google.com/webstore/detail/zapier/ngghlnfmdgnpegcmbpgehkbhkhkbkjpj" )
+
+
+
+Tool.create!(name: "Hubspot", description: "Sales and Marketing software", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSuOgd0HDIOfJbco8VhKHeqYGJWwpwGcM4ESNo_ms_acxeDkJs",
+              website_url: "https://www.hubspot.fr/", chrome_extension_url: "https://chrome.google.com/webstore/detail/hubspot-sales/oiiaigjnkhngdbnoookogelabohpglmd" )
+
+
+Tool.create!(name: "Lusha", description: "B2B contact information at your fingertips", image_url: "https://www.lusha.co/wp-content/uploads/2017/11/LogoDark.svg",
+              website_url: "https://www.lusha.co/", chrome_extension_url: "https://chrome.google.com/webstore/detail/lusha/mcebeofpilippmndlpcghpmghcljajna" )
+
+puts "Seed ends"
