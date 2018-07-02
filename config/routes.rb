48d8c771349b87057dea66e77f9e59221660a269
Victor Lacombe <lacombe.victor@hotmail.fr>
2018-07-02 22:11:06 +0200

    added delete methode and destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tools#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tools, only: [:index, :show, :new, :create, :edit, :update] do
+  resources :tools, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :tags, only: [:new, :create]
   end
 end
